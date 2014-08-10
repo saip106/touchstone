0e2b9529bd7fa339bf6813a0f741e375ee316c3a
Sai Gudigundla <saip106@msn.com>
2014-08-10 15:22:44 -0400

    instakked gulp-jshint; got all code linted

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
+/*jshint node:true*/
+
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', ['test']);
 
 gulp.task('test', [], function () {
     console.log('testing...');
+});
+
+gulp.task('lint', function () {
+    gulp.src(['**/*.js', '!node_modules/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('**/*.spec.js', ['test']);
+    gulp.watch('**/*.js', ['test']);
 });
