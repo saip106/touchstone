62559c7c471b35ed329e321c814904e39d6fc170
Sai Gudigundla <saip106@msn.com>
2014-08-10 15:28:25 -0400

    integrated gulp-mocha

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 
 gulp.task('default', ['test']);
 
 gulp.task('test', ['lint'], function () {
-    
+    return gulp.src('**/*.spec.js', {read: false})
+        .pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('lint', function () {
-    gulp.src(['**/*.js', '!node_modules/**/*.js'])
+    return gulp.src(['**/*.js', '!node_modules/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
