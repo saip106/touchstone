b7a0e4eecb3d64bfef3b35a6d7dd68f8b93815b9
Sai Gudigundla <saip106@msn.com>
2014-08-10 16:43:56 -0400

    got GET functionality working

--- test/touchstone.spec.js
+++ test/touchstone.spec.js
@@ -4,12 +4,20 @@
 'use strict';
 
 var assert = require("assert");
+var touchstone = require('../lib/touchstone');
 
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
-    })
-})
+describe('when accessing touchstone', function () {
+    it('should be defined', function () {
+        assert.notEqual(touchstone, undefined, "undefined");
+        assert.notEqual(touchstone, null, "null");
+        assert.equal(typeof touchstone, 'object', 'not an object');
+    });
+});
+
+describe('when making a GET request to google.com', function() {
+    it('should get a 200 status code', function() {
+        touchstone.get('http://www.google.com', function (response) {
+            assert.equal(response.statusCode, '200', 'Status code is not ok');
+        });
+    });
+});
