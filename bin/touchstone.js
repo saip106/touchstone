5aa0fc3e11b8d2facb5c7b734f3d120eafa357e8
Sai Gudigundla <saip106@msn.com>
2014-08-10 23:37:31 -0400

    got code working by passing the file path as an option

--- bin/touchstone.js
+++ bin/touchstone.js
@@ -4,17 +4,27 @@
 'use strict';
 
 var program = require('commander');
+var helper = require('../lib/helper');
 
 program
     .version('0.0.1')
-    .usage('<keywords>')
+    .usage('[options]')
+    .option('-f, --filepath [filepath]', 'path of the file that contains test json data')
     .parse(process.argv);
 
-if(!program.args.length) {
+if(!program.filepath) {
     program.help();
 }
 else {
-    console.log(process.argv);
+    var testFilePath = program.filepath;
+    console.log(testFilePath);
+    var input = require(testFilePath);
+
+    var nodes = input.nodes;
+    for(var i = 0; i < nodes.length; i++) {
+        helper.get(nodes[i].request.scheme.name + '://' + nodes[i].request.uri.path, function (response) {
+            console.log(response.statusCode);
+        });
+    }
 }
 
-var fileJSON = require('../test/sample.json');
