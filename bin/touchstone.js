aac06ed0671aa1dd4eff6092e8a4f2bd0c6a2b81
Sai Gudigundla <saip106@msn.com>
2014-08-10 23:13:57 -0400

    got npm installation working

--- bin/touchstone.js
+++ bin/touchstone.js
@@ -1,8 +1,20 @@
+#!/usr/bin/env node
 /*jshint node:true*/
 
 'use strict';
 
+var program = require('commander');
 
+program
+    .version('0.0.1')
+    .usage('<keywords>')
+    .parse(process.argv);
+
+if(!program.args.length) {
+    program.help();
+}
+else {
+    console.log(process.argv);
+}
 
 var fileJSON = require('../test/sample.json');
-console.log(JSON.parse(fileJSON));
