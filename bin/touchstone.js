b93c490bdbf5f4fdf0aa39b210fe8ea08852f28a
Sai Gudigundla <saip106@msn.com>
2014-08-11 20:26:52 -0400

    refactored touchstone.js; cleaned up lint errors

--- bin/touchstone.js
+++ bin/touchstone.js
@@ -1,30 +1,50 @@
 #!/usr/bin/env node
 /*jshint node:true*/
 
-'use strict';
+(function () {
+    'use strict';
 
-var program = require('commander');
-var helper = require('../lib/helper');
+    var commander = require('commander');
+    var helper = require('../lib/helper');
 
-program
-    .version('0.0.1')
-    .usage('[options]')
-    .option('-f, --filepath [filepath]', 'path of the file that contains test json data')
-    .parse(process.argv);
-
-if(!program.filepath) {
-    program.help();
-}
-else {
-    var testFilePath = program.filepath;
-    console.log(testFilePath);
-    var input = require(testFilePath);
-
-    var nodes = input.nodes;
-    for(var i = 0; i < nodes.length; i++) {
-        helper.get(nodes[i].request.scheme.name + '://' + nodes[i].request.uri.path, function (response) {
-            console.log(response.statusCode);
-        });
+    function run() {
+        var args = readCommandLineArgs();
+        validate(args);
+        processInputFile(args.filepath);
     }
-}
 
+    function readCommandLineArgs() {
+        commander
+            .version('0.0.1')
+            .usage('[options]')
+            .option('-f, --filepath [filepath]', 'path of the file that contains test json data')
+            .parse(process.argv);
+
+        return {
+            filepath: commander.filepath
+        }
+    }
+
+    function validate(args) {
+        if(!args.filepath) {
+            commander.help();
+            process.exit(1);
+        }
+    }
+
+    function processInputFile(filepath) {
+        var input = require(filepath);
+
+        var nodes = input.nodes;
+
+        function callback(response) {
+            console.log(response.statusCode);
+        }
+
+        for(var i = 0; i < nodes.length; i++) {
+            helper.get(nodes[i].request.scheme.name + '://' + nodes[i].request.uri.path, callback);
+        }
+    }
+
+    run();
+}.call(this));
