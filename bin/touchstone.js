cc16845eac754e1da7240d79e34d74871f476856
Sai Gudigundla <saip106@msn.com>
2014-08-17 17:16:32 -0400

    extracted command line args reader and validator into their own modules

--- bin/touchstone.js
+++ bin/touchstone.js
@@ -6,43 +6,26 @@
 
     var commander = require('commander');
     var helper = require('../lib/helper');
+    var commandLineArgsReader = require('../lib/command-line-args-reader');
+    var validator = require('../lib/args-validator');
 
-    function run() {
-        var args = readCommandLineArgs();
-        validate(args);
+    function run () {
+        var args = commandLineArgsReader.read();
+        validator.validate(args);
         processInputFile(args.filepath);
     }
 
-    function readCommandLineArgs() {
-        commander
-            .version('0.0.1')
-            .usage('[options]')
-            .option('-f, --filepath [filepath]', 'path of the file that contains test json data')
-            .parse(process.argv)
-
-        return {
-            filepath: commander.filepath
-        }
-    }
-
-    function validate(args) {
-        if(!args.filepath) {
-            commander.help();
-            process.exit(1);
-        }
-    }
-
-    function processInputFile(filepath) {
+    function processInputFile (filepath) {
         var input = require(filepath);
         var nodes = input.nodes;
 
-        function callback(error, response, body, options) {
-	        if (!error && response.statusCode === 200) {
-		        console.log(options.url + " success");
-	        }
-	        else {
-		        console.log(options.url + ' failed with error code: ' + response.statusCode);
-	        }
+        function callback (error, response, body, options) {
+            if(!error && response.statusCode === 200) {
+                console.log(options.url + " success");
+            }
+            else {
+                console.log(options.url + ' failed with error code: ' + response.statusCode);
+            }
         }
 
         for(var i = 0; i < nodes.length; i++) {
@@ -50,11 +33,11 @@
             if(nodes[i].type !== 'Request') {
                 continue;
             }
-            
-            if (nodes[i].method.name === 'GET') {
+
+            if(nodes[i].method.name === 'GET') {
                 var options = {
-                    url: helper.parseUrl(nodes[i].uri),
-                    headers: helper.extractHeaders(nodes[i].headers)
+                    url : helper.parseUrl(nodes[i].uri),
+                    headers : helper.extractHeaders(nodes[i].headers)
                 };
                 helper.get(options, callback);
             }
