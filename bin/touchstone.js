0214d7259f2928aa6e5c8c45024db6bccbde3807
Sai Gudigundla <saip106@msn.com>
2014-08-17 17:21:35 -0400

    extracted web requests processor

--- bin/touchstone.js
+++ bin/touchstone.js
@@ -5,43 +5,14 @@
     'use strict';
 
     var commander = require('commander');
-    var helper = require('../lib/helper');
     var commandLineArgsReader = require('../lib/command-line-args-reader');
     var validator = require('../lib/args-validator');
+    var webRequestsProcessor = require('../lib/web-requests-processor');
 
     function run () {
         var args = commandLineArgsReader.read();
         validator.validate(args);
-        processInputFile(args.filepath);
-    }
-
-    function processInputFile (filepath) {
-        var input = require(filepath);
-        var nodes = input.nodes;
-
-        function callback (error, response, body, options) {
-            if(!error && response.statusCode === 200) {
-                console.log(options.url + " success");
-            }
-            else {
-                console.log(options.url + ' failed with error code: ' + response.statusCode);
-            }
-        }
-
-        for(var i = 0; i < nodes.length; i++) {
-
-            if(nodes[i].type !== 'Request') {
-                continue;
-            }
-
-            if(nodes[i].method.name === 'GET') {
-                var options = {
-                    url : helper.parseUrl(nodes[i].uri),
-                    headers : helper.extractHeaders(nodes[i].headers)
-                };
-                helper.get(options, callback);
-            }
-        }
+        webRequestsProcessor.process(args.filepath);
     }
 
     run();
